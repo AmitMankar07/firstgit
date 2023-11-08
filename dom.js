945061ae68c79e9615a8843b859a3b76590fca2a
AmitMankar07 <amitmankar071@gmail.com>
2023-11-08 22:36:25 +0530

    traversing the dom

--- dom.js
+++ dom.js
@@ -56,11 +56,75 @@
 
 //query All
 
-var scnditem=document.querySelectorAll('.list-group-item');
-scnditem[1].style.color='green';
+// var scnditem=document.querySelectorAll('.list-group-item');
+// scnditem[1].style.color='green';
 
-var odditems=document.querySelectorAll('li:nth-child(odd)');
+// var odditems=document.querySelectorAll('li:nth-child(odd)');
 
-for(let i=0;i<odditems.length;i++){
-    odditems[i].style.backgroundColor='green';
-}
+// for(let i=0;i<odditems.length;i++){
+//     odditems[i].style.backgroundColor='green';
+// }
+
+
+
+//traversing the DOM
+
+var itemList=document.querySelector('#items');
+//parent node
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor='#f4f4f4';
+console.log(itemList.parentNode.parentNode);
+
+//parent elemnt
+console.log(itemList.parentElement);
+itemList.parentNode.style.backgroundColor='#f4f4f4';
+console.log(itemList.parentElement.parentElement);
+
+//child node
+console.log(itemList.childNodes);
+
+console.log(itemList.children[1]);
+itemList.children[1].style.color='red';
+
+//first child
+console.log(itemList.firstChild);
+console.log(itemList.firstElementChild);
+
+//last child
+console.log(itemList.lastChild);
+console.log(itemList.lastElementChild);
+
+//nextsibling
+console.log(itemList.nextSibling);
+console.log(itemList.nextElementSibling);
+
+//previousSibling
+
+console.log(itemList.previousSibling);
+console.log(itemList.previousElementSibling);
+
+//create element
+
+var newDiv=document.createElement('div');
+
+newDiv.id='hel';
+
+newDiv.setAttribute('title','hello div');
+
+var newdivtext=document.createTextNode('hello world');
+newDiv.append(newdivtext);
+
+
+
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+
+container.insertBefore(newDiv,h1);
+
+
+var newcontainer=document.getElementById('main');
+
+var listgroup=document.getElementById('items');
+
+console.log("-------------",newcontainer,listgroup);
+newcontainer.insertBefore(newDiv,listgroup);
