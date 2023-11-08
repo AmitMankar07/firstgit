c483d845d58aa5b4ab0f0921885d7ea2abc09734
AmitMankar07 <amitmankar071@gmail.com>
2023-11-08 19:30:26 +0530

    changes using tag name

--- dom.js
+++ dom.js
@@ -8,19 +8,41 @@
 
 // console.log(document.getElementById('header-title'));
 
-var headertitle=document.getElementById('header-title');
-var header=document.getElementById('main-header')
-console.log(headertitle);
+// var headertitle=document.getElementById('header-title');
+// var header=document.getElementById('main-header')
+// console.log(headertitle);
 
-header.style.border='solid 3px black';
+// header.style.border='solid 3px black';
 
-var items=document.querySelector('.title');
-items.style.fontweight='bold';
-items.style.color='green';
+// var items=document.querySelector('.title');
+// items.style.fontweight='bold';
+// items.style.color='green';
 
-var itemgroup=document.getElementsByClassName('list-group-item');
-itemgroup[2].style.backgroundColor='green';
+// var itemgroup=document.getElementsByClassName('list-group-item');
+// itemgroup[2].style.backgroundColor='green';
+
+// for(let i=0;i<itemgroup.length;i++){
+//     itemgroup[i].style.fontWeight='bold '
+// }
+
+//tagname
+
+//using clasname
+// var itemgroup=document.getElementsByClassName('list-group-item');
+// itemgroup[2].style.backgroundColor='yellow';
+
+// for(let i=0;i<itemgroup.length;i++){
+//     itemgroup[i].style.color='red';
+// }
+
+//now by using tagname
+
+var itemgroup=document.getElementsByTagName('li');
+itemgroup[2].style.backgroundColor='yellow';
 
 for(let i=0;i<itemgroup.length;i++){
-    itemgroup[i].style.fontWeight='bold '
+    itemgroup[i].style.color='red';
 }
+
+
+
