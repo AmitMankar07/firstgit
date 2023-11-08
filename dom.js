a4c3bba14b5d3cc7bba6730793ab1fec82511b07
AmitMankar07 <amitmankar071@gmail.com>
2023-11-08 18:52:51 +0530

    items changes

---
+++ dom.js
@@ -0,0 +1,19 @@
+// console.log(document.domain);
+// console.dir(document);
+
+// console.log(document.URL);
+// console.log(document.body);
+
+// console.log(document.head);
+
+// console.log(document.getElementById('header-title'));
+
+var headertitle=document.getElementById('header-title');
+var header=document.getElementById('main-header')
+console.log(headertitle);
+
+header.style.border='solid 3px black';
+
+var items=document.querySelector('.title');
+items.style.fontweight='bold';
+items.style.color='green';
